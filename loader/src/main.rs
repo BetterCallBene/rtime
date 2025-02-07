98897115daf101616e89d106cb97dab287df92ca
--get <benedikt.koenig@agile-robots.com>
2025-02-07 14:44:58 +0100

    feat(loader): add helper functions for subscribing, and getting values from blackboard

--- loader/src/main.rs
+++ loader/src/main.rs
@@ -1,16 +1,23 @@
+mod components;
+mod config;
 mod helper;
 mod rtlibrary;
-mod config;
-mod components;
 use clap::Parser;
-use helper::{create_library_name, load_library, plugin_dir};
+use components::{create_caps, Components, ComponentsType};
 use config::{LibraryConfigs, RTConfig};
+use crossbeam_channel::{unbounded, Receiver, Sender};
+use helper::{create_library_name, load_library, plugin_dir};
+use interfaces::capabilities::Function;
+use lazy_static::lazy_static;
+use log::{debug, error, info, warn};
 use rtlibrary::RTLibrary;
-use components::{Components, create_caps};
-use log::{info, warn, error};
-use std::{path::PathBuf, sync::Arc};
-use tokio::time::{self, Duration as dur};
+use std::{
+    ffi::{c_char, c_int, c_void, CStr},
+    path::PathBuf,
+    sync::Arc,
+};
 use tokio::signal;
+use tokio::time::{self, Duration as dur};
 
 #[derive(Parser, Debug)]
 #[command(version = "0.1.0", about = "Kiss Runtime")]
@@ -18,7 +25,22 @@ struct Args {
     config: PathBuf,
 }
 
-fn load_libraries(config: &LibraryConfigs) -> Vec<RTLibrary>{
+struct SenderReceiver {
+    sender: Sender<String>,
+    receiver: Receiver<String>,
+}
+
+lazy_static! {
+    static ref TASK_SENDER_RECV: SenderReceiver = {
+        let (sender, receiver) = unbounded::<String>();
+        SenderReceiver {
+            sender: sender,
+            receiver: receiver,
+        }
+    };
+}
+
+fn load_libraries(config: &LibraryConfigs) -> Vec<RTLibrary> {
     info!("Load libraries...");
     let mut libraries: Vec<RTLibrary> = Vec::new();
 
@@ -53,12 +75,12 @@ fn load_libraries(config: &LibraryConfigs) -> Vec<RTLibrary>{
                         let library_name = rtlibrary.summary.name.clone();
 
                         let found = libraries.iter().find(|lib| lib.name() == library_name);
-                    
+
                         if found.is_some() {
                             warn!("Library '{}' already loaded. Skip loading.", library_name);
                             continue;
                         }
-                        
+
                         libraries.push(rtlibrary);
                     }
                     Err(e) => {
@@ -74,25 +96,95 @@ fn load_libraries(config: &LibraryConfigs) -> Vec<RTLibrary>{
     libraries
 }
 
-async fn task_manager(compoents: Arc<Components>) {
+fn create_caps_blackboard(
+    library_list: &Vec<ComponentsType>,
+) -> interfaces::capabilities::Capabilities {
+    let requires = vec!["blackboard".to_string()];
+    create_caps(&requires, library_list)
+}
+
+fn subscribe_to_blackboard(
+    caps: &interfaces::capabilities::Capabilities,
+    key: &str,
+    callback: extern "C" fn(*const c_char) -> c_int,
+) -> Result<(), String> {
+    let subscribe_cap = caps.get("blackboard_subscribe");
+
+    if subscribe_cap.is_none() {
+        return Err("Blackboard is not available".to_string());
+    }
+    let subscribe_fn: Function<
+        extern "C" fn(*const c_char, *const c_char, callback: *mut c_void) -> c_int,
+    > = unsafe { subscribe_cap.unwrap().get().unwrap() };
+
+    let key = key.as_ptr() as *const c_char;
+    let callback = callback as *mut c_void;
+
+    let result = subscribe_fn(key, "loader\0".as_ptr() as *const c_char, callback);
+    if result != 0 {
+        return Err("Failed to subscribe to blackboard".to_string());
+    }
+    return Ok(());
+}
+
+fn get_string_from_blackboard(
+    caps: &interfaces::capabilities::Capabilities,
+    key: &str,
+) -> Result<String, String> {
+    let get_string_cap = caps.get("blackboard_get_string");
+
+    if get_string_cap.is_none() {
+        return Err("Blackboard is not available".to_string());
+    }
+
+    let get_string_fn: Function<unsafe extern "C" fn(ckey: *const c_char, cvalue: *mut c_char) -> c_int> =
+        unsafe { get_string_cap.unwrap().get().unwrap() };
+
+    let key = key.as_ptr() as *const c_char;
+    let result = unsafe{get_string_fn(key, std::ptr::null_mut())};
+
+    if result < 0 {
+        return Err("Failed to get string from blackboard".to_string());
+    }
+    
+    let mut buffer = vec![0u8; result as usize];
+
+    let result = unsafe{get_string_fn(key, buffer.as_mut_ptr() as *mut c_char)};
+    if result < 0 {
+        return Err("Failed to get string from blackboard".to_string());
+    }
+
+    let result = unsafe {CStr::from_ptr(buffer.as_ptr() as *const c_char).to_str().map_err(|e| e.to_string())}?;
+    Ok(result.to_string())
+}
+
+async fn runner(content: String) {
+    
+    
+}
+
+async fn task_manager(compoents: Arc<Components>) -> Result<(), String> {
     let mut interval = time::interval(dur::from_millis(100));
 
-    let requires = vec!["blackboard".to_string()];
-    let caps = create_caps(&requires, &compoents.inner);
-
+    let caps = create_caps_blackboard(&compoents.inner);
     let string_get_cap = caps.get("blackboard_get_string");
-    
+
     if string_get_cap.is_none() {
         //panic!("Capability 'blackboard_set_string' not found");
         error!("Blackboard is not available");
-        return;
+        return Err("Blackboard is not available".to_string());
     }
-    
-    loop {
 
+    loop {
+        let key = TASK_SENDER_RECV.receiver.try_recv();
+        if key.is_ok() {
+            let content = get_string_from_blackboard(&caps, "start_project\0").unwrap();
+            tokio::spawn(runner(content));
+        }
         interval.tick().await;
     }
 }
+
 
 
 #[tokio::main]
@@ -118,13 +210,22 @@ async fn main() -> Result<(), String> {
     let config: RTConfig = serde_yml::from_str(&config_str)
         .map_err(|e| format!("Failed to parse config: {}. Reason: {}", config_str, e))?;
 
-
     let libraries = load_libraries(&config.libraries);
     let components = Components::new(libraries);
     components.start_services();
 
     let components = Arc::new(components);
     let task_handle = tokio::spawn(task_manager(components.clone()));
+
+    extern "C" fn notify_callback(key: *const c_char) -> c_int {
+        let key = unsafe { CStr::from_ptr(key).to_str().unwrap() };
+        debug!("Callback called for key: {}", key);
+        TASK_SENDER_RECV.sender.send(key.to_string()).unwrap();
+        0
+    }
+
+    let caps = create_caps_blackboard(&components.inner);
+    subscribe_to_blackboard(&caps, "start_project\0", notify_callback)?;
 
     // Wait for Ctrl+C signal
     tokio::select! {
@@ -141,10 +242,10 @@ async fn main() -> Result<(), String> {
 
 #[cfg(test)]
 mod tests {
-    use super::*;
-    use serial_test::serial;
     use super::config::LibraryConfig;
+    use super::*;
     use interfaces::blackboard::BlackboardEntries;
+    use serial_test::serial;
 
     impl LibraryConfig {
         fn new(name: &str, path: Option<PathBuf>, attributes: Option<BlackboardEntries>) -> Self {
@@ -155,7 +256,7 @@ mod tests {
             }
         }
     }
-    
+
     #[serial]
     #[test_log::test]
     fn test_load_libraries() {
@@ -171,7 +272,10 @@ mod tests {
     #[serial]
     #[test_log::test]
     fn test_load_library_twice() {
-        let config = vec![LibraryConfig::new("blackboard", None, None), LibraryConfig::new("blackboard", None, None)];
+        let config = vec![
+            LibraryConfig::new("blackboard", None, None),
+            LibraryConfig::new("blackboard", None, None),
+        ];
 
         let libraries = load_libraries(&config);
         assert_eq!(libraries.len(), 1);
@@ -194,7 +298,7 @@ mod tests {
         let components = Components::new(libraries);
         assert_eq!(components.inner.len(), 1);
     }
-    
+
     // #[serial]
     // #[test_log::test]
     // fn test_
@@ -202,8 +306,10 @@ mod tests {
     #[serial]
     #[test_log::test]
     fn test_create_caps() {
-        
-        let config = vec![LibraryConfig::new("blackboard", None, None), LibraryConfig::new("webinterface", None, None)];
+        let config = vec![
+            LibraryConfig::new("blackboard", None, None),
+            LibraryConfig::new("webinterface", None, None),
+        ];
 
         let libraries = load_libraries(&config);
         assert_eq!(libraries.len(), 2);
@@ -223,7 +329,6 @@ mod tests {
         assert!(string_set_cap.is_some());
         // let string_set_cap = string_set_cap.unwrap();
 
-        
         // let result = unsafe {
         //     let f: Function<unsafe extern "C" fn(*const c_char, *const c_char) -> c_int> = string_set_cap.get().unwrap();
         //     f("example_key\0".as_ptr() as *const c_char, "test\0".as_ptr() as *const c_char)
